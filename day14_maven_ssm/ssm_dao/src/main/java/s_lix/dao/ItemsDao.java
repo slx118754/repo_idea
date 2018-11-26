d580b8f55d94c99b11b8c16d504158cb89d24c31
lix <1187546163@qq.com>
2018-11-26 19:21:38 +0800

    div1 ssm 项目 修改

--- day14_maven_ssm/ssm_dao/src/main/java/s_lix/dao/ItemsDao.java
+++ day14_maven_ssm/ssm_dao/src/main/java/s_lix/dao/ItemsDao.java
@@ -3,7 +3,11 @@ package s_lix.dao;
 import org.apache.ibatis.annotations.Select;
 import s_lix.domain.Items;
 
+import java.util.List;
+
 public interface ItemsDao {
     @Select("select * from items where id = #{id} ")
     Items findOneById(int id);
+
+    List<Items> findAll();
 }
